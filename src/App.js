b525a27bea18c13bcce457bcaec37e5bd1e904fd
Cristopher Yates <cmoyates@gmail.com>
2021-04-05 04:11:28 -0230

    Fixed router bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
+import {Route, HashRouter as Router, Switch} from "react-router-dom";
 import Home from "./pages/Home";
 import About from './pages/About';
 
